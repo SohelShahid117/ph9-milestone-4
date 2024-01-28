19df16c8869043831c1052f6ef05e80f6dd96e1d
SohelShahid117 <sohelshahid09@gmail.com>
2024-01-28 09:49:47 +0600

    complete up to w3-js var

---
+++ module-17.5/array.js
@@ -0,0 +1,33 @@
+let number = [1,2,3,6,7,33,22,11];
+console.log(number);
+console.log(number.length);
+console.log(number[0]);
+console.log(number[3]);
+const fifthNumberValue = number[4];
+console.log(fifthNumberValue);
+
+console.log(number);
+number[0] = 999;
+console.log(number);
+
+const ages = [];
+// number.push = 66;
+console.log(number);
+number.push(668);
+number.push(8);
+number.push(90)
+number.push(69);;
+console.log(number);
+number.pop();
+console.log(number);
+const out2 = number.pop();
+console.log(number);
+console.log(out2);
+
+console.log(number);
+number.shift();
+console.log(number);
+
+console.log(number);
+number.unshift(887);
+console.log(number);
