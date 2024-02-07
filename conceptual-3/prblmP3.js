e66cfa18e78e2473072e6610cdb3032ebc5a728a
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-07 19:07:56 +0600

    complete the conceptual part-3

--- conceptual-3/prblmP3.js
+++ conceptual-3/prblmP3.js
@@ -17,4 +17,34 @@ console.log(pandaCost(2, 1, 2))
 console.log(pandaCost(-2, 1, 2))
 
 //prblm-3
-//from-11 min
+function picnicBudget(picnicParticipants) {
+    if (typeof picnicParticipants !== `number` || picnicParticipants < 0) {
+        return `Participants should be valid a positive number`
+    }
+    let first100Cost = 0;
+    let second101To200Cost = 0;
+    let greater200Cost = 0;
+    let unitCostForFirst100 = 5000;
+    let unitCostFor101To200 = 4000;
+    let unitCostForGreaterThan200 = 3000;
+    if (picnicParticipants <= 100) {
+        first100Cost = picnicParticipants * unitCostForFirst100;
+        return first100Cost;
+    }
+    else if (picnicParticipants <= 200) {
+        first100Cost = 100 * unitCostForFirst100;
+        second101To200Cost = (picnicParticipants - 100) * unitCostFor101To200;
+        const totalCost = first100Cost + second101To200Cost;
+        return totalCost;
+    }
+    else {
+        first100Cost = 100 * unitCostForFirst100;
+        second101To200Cost = 100 * unitCostFor101To200;
+        greater200Cost = (picnicParticipants - 200) * unitCostForGreaterThan200;
+        const totalCost = first100Cost + second101To200Cost + greater200Cost;
+        return totalCost;
+    }
+}
+console.log(picnicBudget(201));
+
+
