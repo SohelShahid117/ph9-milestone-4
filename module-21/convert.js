5e4a2ca5d795cbab93bc9604209d75426c3b5033
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-07 19:23:21 +0600

    codin on module 21-2 & next start from this vdo - 8 min

--- module-21/convert.js
+++ module-21/convert.js
@@ -1,4 +1,20 @@
-// vdo-21-2:inch to feet , miles to km
+// vdo-21-2:inch to feet , miles to km-this vdo for sumi
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet;
+}
+const shuvoHeight = inchToFeet(67);
+console.log(shuvoHeight);
+
+function inchToFeet2(inch) {
+    const feet = parseInt(inch / 12);
+    const inchee = inch % 12;
+    return `shuvo is ${feet} feet & ${inchee} inch`;
+}
+const shuvoHeight2 = inchToFeet2(67);
+console.log(shuvoHeight2);
+
+//start from 8 min
 
 // vdo-21-3
 // vdo-21-4
